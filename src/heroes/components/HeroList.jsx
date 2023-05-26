1c75eef49e2d1344c8e473e63b4c0bc5e5285e0a
Juan_Corso <est.juan.corso@unimilitar.edu.co>
2023-05-25 22:48:59 -0400

    Terjetas de super Heroes Completadas

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,11 +1,12 @@
 import { getHeoesByPublisher } from "../helpers";
+import { HeroCard } from "./";
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeoesByPublisher(publisher);
 
   const listItems = heroes.map((heroe) => (
-    <li key={heroe.id}>{heroe.superhero}</li>
+    <HeroCard key={heroe.id} {...heroe} />
   ));
 
-  return <ul>{listItems}</ul>;
+  return <div className="row rows-cols-1 row-cols-md-3 g-3">{listItems}</div>;
 };
